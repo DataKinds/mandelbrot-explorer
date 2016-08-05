1e7b9686f9150c69d7a1e9b52fc84105a9b6604a
Tyler L <tslimkemann42@gmail.com>
2016-08-05 11:45:02 -0700

    simplified mandel

--- fragShader.glsl
+++ fragShader.glsl
@@ -1,12 +1,9 @@
 #version 330
+precision highp float;
 out vec4 frag_color;
 in vec4 gl_FragCoord;
 uniform ivec2 screenDims;
 uniform uint frameNum;
-precision highp float;
-float magnitude(float real, float imaginary) {
-    return sqrt(pow(real, 2.0) + pow(imaginary, 2.0));
-}
 
 int iterationsToEscape(float x, float y) {
     float a = 0;
@@ -18,10 +15,10 @@ int iterationsToEscape(float x, float y) {
     //imaginary part (b) = (2abi)
     //full formula:
     //nextIteration (a and b) = lastIteration (a and b)^2 + c (x and y)
-    while ((magnitude(a, b) < 2.0) && iterations < 100) {
-        a = (pow(a, 2.0) - pow(b, 2.0)) + x;
-        //b = (2*a*b) + y;
-        b = ((mod(float(frameNum)/300.0, 3.0) + 1)*a*b) + y;
+    while (((a * a + b * b) < 4.0) && iterations < 100) {
+        a = (a * a - b * b) + x;
+        b = (2 * a * b) + y;
+        //b = ((mod(float(frameNum)/100.0, 5.0) + 1)*a*b) + y;
         iterations++;
     }
     return iterations;
@@ -29,10 +26,10 @@ int iterationsToEscape(float x, float y) {
 
 void main() {
     //0.0 to 4.0
-    vec2 fractalCoords = gl_FragCoord.xy / (screenDims.xy / 4.0);
+    vec2 fractalCoords = (gl_FragCoord.xy * 4.0) / screenDims.xy;
     //-2.0 to 2.0
     fractalCoords = fractalCoords - 2.0;
-    vec3 shade = vec3(iterationsToEscape(fractalCoords.x, fractalCoords.y) / 100.0);
+    vec3 shade = vec3(iterationsToEscape(fractalCoords.x, fractalCoords.y) / 150.0);
     //ivec2 fragInt = ivec2(int(gl_FragCoord.x), int(gl_FragCoord.y));
     //if ((fragInt.x ^ fragInt.y) == mod(frameNum, 256)) {
     //    shade = vec3(1.0);
