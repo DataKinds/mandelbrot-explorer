02c9472730b2281954865f764bc68e01add3e626
Tyler L <tslimkemann42@gmail.com>
2016-08-04 22:05:03 -0700

    sierpinski

--- fragShader.glsl
+++ fragShader.glsl
@@ -4,6 +4,11 @@ in vec4 gl_FragCoord;
 uniform ivec2 screenDims;
 uniform uint frameNum;
 void main () {
-    if ()
+    vec3 shade = vec3(0.0);
+    ivec2 fragInt = ivec2(int(gl_FragCoord.x), int(gl_FragCoord.y));
+    if ((fragInt.x & fragInt.y) == 0) {
+        shade = vec3(1.0);
+    } else {
+    }
     frag_color = vec4(shade, 1.0);
 }
